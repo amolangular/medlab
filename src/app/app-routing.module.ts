75a910b6eaeda330029bccbb1762d676697a6c77
amolg <amolg.angular@gmail.com>
2024-04-10 21:11:57 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MedicinesHomeComponent } from './components/medicines-home/medicines-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'medicine',component:MedicinesHomeComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
